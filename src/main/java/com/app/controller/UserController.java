7530a2a312333f2c2551064577d1eb89fd60ddbd
User <User@TOSHIBA>
2022-10-28 01:37:56 +0530

    first github account

---
+++ src/main/java/com/app/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.app.entity.User;
+
+@Controller
+public class UserController {
+	
+	@GetMapping("/")
+	public String login(Model model) {
+		User user=new User();
+	model.addAttribute("User" ,user);
+		return "login";
+	}
+
+}
